9b9002c86dc3f1e57713a8811649869846d8db10
Jeroen van Hoof <j.m.a.p.v.hoof@student.tue.nl>
2014-04-17 20:03:44 +0200

    code cleanup

--- beta-2/js/scroller.js
+++ beta-2/js/scroller.js
@@ -1,78 +1,128 @@
-var ids = new Array();
+/* Scroller.js
+ * 
+ * Author: Jeroen van Hoof
+ */
+
+var SCROLLTIME = 400;
+var ids = [];
 
 // Returns the top and bottom offset of an element.
 function position(elem) {
-	var top = elem.offset().top;
-	var bottom = top + elem.outerHeight();
+    "use strict";
 
-	return [top, bottom]
+    var top, bottom;
+    top = elem.offset().top;
+    bottom = top + elem.outerHeight();
+
+    return [top, bottom];
 }
 
 // Returns the position of a navigation link.
 function listPosition(id) {
-	var li = $(".nav li a[href='#" + id + "']");
+    "use strict";
 
-	return position(li);
+    var li = $(".nav li a[href='#" + id + "']");
+
+    return position(li);
 }
 
 // Returns the position of a box.
 function boxPosition(id) {
-	var box = $("#" + id);
+    "use strict";
 
-	return position(box);
+    var box = $("#" + id);
+
+    return position(box);
 }
 
 // Returns true if the navigation link is at the same height as the box. False otherwise.
 function checkBetween(id) {
-	var listPos = listPosition(id);
-	var boxPos = boxPosition(id);
+    "use strict";
 
-	var topLink = listPos[0];
-	var bottomLink = listPos[1];
-	var topBox = boxPos[0];
-	var bottomBox = boxPos[1];
+    var listPos, boxPos, topLink, bottomLink, topBox, bottomBox;
+    listPos = listPosition(id);
+    boxPos = boxPosition(id);
+    topLink = listPos[0];
+    bottomLink = listPos[1];
+    topBox = boxPos[0];
+    bottomBox = boxPos[1];
 
-	if (topLink >= topBox && bottomLink <= bottomBox) {
-		return true;
-	} else {
-		return false;
-	}
+    if (topLink >= topBox && bottomLink <= bottomBox) {
+        return true;
+    }
+
+    return false;
 }
 
 // Activates a navigation link when it has the same height as the box.
 function activator(id) {
-	var li = $(".nav li a[href='#" + id + "']");
-	if (checkBetween(id)) {
-		li.parent().addClass('active');
-	} else {
-		li.parent().removeClass('active');
-	}
+    "use strict";
+
+    var li = $(".nav li a[href='#" + id + "']");
+    if (checkBetween(id)) {
+        li.parent().addClass('active');
+    } else {
+        li.parent().removeClass('active');
+    }
 }
 
-// Get all the ids inside the target
+// Find all siblings with an id, and store this id.
 function getIds(target) {
-	$(target + " div").each(function(index, element){
-		if ($(element).attr("id") !== undefined) {
-			ids.push($(element).attr("id"));
-		}
-	});
+    "use strict";
+
+    $(target + " div").each(function (index, element) {
+        if ($(element).attr("id") !== undefined) {
+            ids.push($(element).attr("id"));
+        }
+    });
 }
 
-// Check if element has to be activated.
-function check() {
-	for (var i = 0; i < ids.length; i++) {
-		activator(ids[i]);
-	}
+// Update each element.
+function update() {
+    "use strict";
+
+    var i;
+    for (i = 0; i < ids.length; i += 1) {
+        activator(ids[i]);
+    }
 }
 
-$(document).ready(function(){
-	// Get ids.
-	getIds(".container");
+// Main function.
+function scroller(target) {
+    "use strict";
 
-	$(document).on("scroll", function() {
-		check();
-	});
-	$(".menu").on("scroll", function() {
-		check();
-	});
-});
+    getIds(target);
+
+    $(document).on("scroll", function () {
+        update();
+    });
+
+    // Also update if the menu itself is scrolled.
+    $(".menu").on("scroll", function () {
+        update();
+    });
+}
+
+// Optional autoScroll function: if clicked on link, autoscroll to id.
+function autoScroll(target) {
+    "use strict";
+
+    $(target).on("click", function (e) {
+        e.preventDefault();
+        var scrollTarget = $($(this).attr("href"));
+        $('html,body').animate({
+            scrollTop: scrollTarget.offset().top
+        }, SCROLLTIME);
+    });
+}
+
+// Optional scrollTop function: scroll to top, if clicked on target.
+function scrollTop(target) {
+    "use strict";
+
+    $(target).on("click", function () {
+        $('html,body').animate({
+            scrollTop: "0px"
+        }, SCROLLTIME);
+    });
+}
