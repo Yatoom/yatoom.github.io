b599ecfb8304de7ee3b269151d93623058749711
Jeroen van Hoof <j.m.a.p.v.hoof@student.tue.nl>
2014-04-18 20:27:00 +0200

    Open/close menu.

--- beta-2/js/control.js
+++ beta-2/js/control.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
 	scrollTop('.logo');
 	autoScroll('.menu a');
 	autoScroll('.autoscroll');
+
+	$('.menu-button').on("click", function () {
+		if ($('.menu').attr('style') !== undefined) {
+			$('.menu-button, .menu, .container, .left, .design').removeAttr("style");
+			$('.menu-button').removeClass('button-left');
+		} else {
+			$('.menu, .design').css("left", "0px");
+			$('.container').css("left", "255px");
+			$('.menu-button').css("left", "calc(255px)");
+			$('.menu-button').addClass('button-left');
+		}
+	});
 });
