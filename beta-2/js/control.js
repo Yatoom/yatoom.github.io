ce36cfdf487d8f740103f0f09391a0d821e28119
Jeroen van Hoof <j.m.a.p.v.hoof@student.tue.nl>
2014-04-18 21:36:30 +0200

    Javascript compatibility Android browser

--- beta-2/js/control.js
+++ beta-2/js/control.js
@@ -6,12 +6,16 @@ $(document).ready(function(){
 
 	$('.menu-button').on("click", function () {
 		if ($('.menu').attr('style') !== undefined) {
-			$('.menu-button, .menu, .container, .left, .design').removeAttr("style");
-			$('.menu-button').removeClass('button-left');
+			// $('.menu-button, .menu, .container, .left, .design').removeAttr("style");
+			// $('.menu-button').removeClass('button-left');
+
+			$('.menu, .design').css("left", "-255px");
+			$('.container').css("left", "0px");
+			$('.menu-button').css("left", "0px");
 		} else {
 			$('.menu, .design').css("left", "0px");
 			$('.container').css("left", "255px");
-			$('.menu-button').css("left", "calc(255px)");
+			$('.menu-button').css("left", "255px");
 			$('.menu-button').addClass('button-left');
 		}
 	});
